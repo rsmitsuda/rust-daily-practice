70b1c743ef76a432df3abda567480deb566a1bd1
root <root@localhost.localdomain>
2020-03-17 03:12:02 +0000

    created the memoization for subsequence, no stepback

--- daily/3_16_20/src/main.rs
+++ daily/3_16_20/src/main.rs
@@ -14,16 +14,45 @@ fn print_results(results: Vec<Vec<i64>>) {
 fn find_subsequence(input1: Vec<i64>, input2: Vec<i64>) -> Vec<i64> {
 	let mut ret_subsequence: Vec<i64> = Vec::new();
 	let mut memoize = vec![vec![0; input2.len() + 1]; input1.len() + 1];
+	let (mut cur_i, mut cur_j): (i64, i64);
 
-	//put in separate initialization function
-	for i in 0..input1.len() {
-		for j in 0..input2.len() {
-			if j == 0 {
-				memoize[i + 1][j] = input1[i]
-			}
+	for i in 1..memoize.len() {
+		// cur_i = memoize[i][0];
+		cur_i = input1[i - 1];
+		for j in 1..memoize[0].len() {
+			// cur_j = memoize[0][j];
+			cur_j = input2[j - 1];
 
-			if i == 0 {
-				memoize[i][j + 1] = input2[j]; 
+			print!("({:?}, {:?}) \n", cur_i, cur_j);
+
+			if cur_i == cur_j {
+					if memoize[i - 1][j] > memoize[i][j - 1] {
+						if memoize[i - 1][j - 1] > memoize[i - 1][j] {
+							memoize[i][j] = memoize[i - 1][j - 1] + 1;
+						} else {
+							memoize[i][j] = memoize[i - 1][j] + 1;
+						}
+					} else {
+						if memoize[i - 1][j - 1] > memoize[i][j - 1] {
+							memoize[i][j] = memoize[i - 1][j - 1] + 1;
+						} else {
+							memoize[i][j] = memoize[i][j - 1] + 1;
+						}
+					}
+			} else {
+					if memoize[i - 1][j] > memoize[i][j - 1] {
+						if memoize[i - 1][j - 1] > memoize[i - 1][j] {
+							memoize[i][j] = memoize[i - 1][j - 1];
+						} else {
+							memoize[i][j] = memoize[i - 1][j];
+						}
+					} else {
+						if memoize[i - 1][j - 1] > memoize[i][j - 1] {
+							memoize[i][j] = memoize[i - 1][j - 1];
+						} else {
+							memoize[i][j] = memoize[i][j - 1];
+						}
+					}
 			}
 		}
 	}
@@ -34,7 +63,7 @@ fn find_subsequence(input1: Vec<i64>, input2: Vec<i64>) -> Vec<i64> {
 }
 
 fn main() {
-	let vec1 = vec![1, 2, 3, 4, 5];
-	let vec2 = vec![6, 7, 8];
+	let vec1 = vec![1, 2, 20, 23, 3, 24];
+	let vec2 = vec![1, 20, 23, 24];
 	find_subsequence(vec1, vec2);
 }
