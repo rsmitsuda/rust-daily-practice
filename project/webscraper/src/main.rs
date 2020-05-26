5ac22e01f23f05703482ad2031eea3704925fe0f
root <root@localhost.localdomain>
2020-05-26 01:34:40 +0000

    fetch league champions

--- project/webscraper/src/main.rs
+++ project/webscraper/src/main.rs
@@ -3,54 +3,46 @@ extern crate select;
 extern crate scraper;
 extern crate tokio;
 
-// use scraper::Html;
+use scraper::{Html, Selector};
 // use select::predicate::{Predicate, Attr, Class, Name};
-// use select::predicate::Name;
+use select::predicate::Name;
 use select::document::Document;
-use std::fs::File;
-use std::convert::From;
+// use std::fs::File;
 
 async fn get_website(url: &str) -> Result<String, reqwest::Error> {
     let ret = reqwest::get(url).await?.text().await?;
     let ret_as_bytes = ret.as_bytes();
 
-    print!("UTFL {:?}\n", String::from_utf8(ret_as_bytes.to_vec()));
+    // print!("UTFL {:?}\n", String::from_utf8(ret_as_bytes.to_vec()));
 
     let ret_str = ret.to_string();
     // file_str.write_all(ret.unwrap());
 
     let response = reqwest::get(url).await?;
 
+	let document = Document::from_read(ret_as_bytes);
+    let doc_links = document.unwrap()
+    	.find(Name("a"))
+    	.filter_map(|links| links.attr("href"))
+    	.for_each(|x| println!("{}", x));
 
-    // let doc = Html::parse_document(&response);
+    let temp_document = Html::parse_document(&ret);
+    let champions = Selector::parse(".champion-index-table__name").unwrap();
 
-    // for node in response.find(Attr("href", "id")) {
-    // 	print!("menu: {:?}\n", node.text());
-    // }
-
-    Document::from_read(ret_as_bytes);
-
-    // print!("response: {:?}\n", response1);
-    // print!("text: {:?}\n", response2);
-    // print!("documetn: {:?}\n", doc);
-
-    // let mut response = match reqwest::get(url).await?.text().await? {
-    // 	Ok(good) => return Ok(()),
-    // 	Err(e) => return Err(e)
-    // };
-
-
+    for champion in temp_document.select(&champions) {
+    	let champ_text = champion.text().collect::<Vec<_>>();
+    	println!("{:?}", champ_text);
+    }
 
     return Ok(ret);
 }
 
 #[tokio::main]
 async fn main() {
-    // let url = "https://www.rust-lang.org";
-    let url = "https://news.ycombinator.com";
+    let url = "https://www.rust-lang.org";
+    let league_url = "https://na.op.gg/champion/statistics";
 
-
-    let response = get_website(&url).await;
+    let response = get_website(&league_url).await;
 
     // print!("response: {:?}\n", response);
 
