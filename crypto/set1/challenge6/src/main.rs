db9fc670b00adbb685ee6a46da6c4e3cf525a12b
root <root@localhost.localdomain>
2020-07-14 04:34:32 +0000

    hamming distance complete

--- crypto/set1/challenge6/src/main.rs
+++ crypto/set1/challenge6/src/main.rs
@@ -1,3 +1,5 @@
+use base64;
+
 static BIT_MASKS: [u8; 8] = [
 	0x01, //0x01 00000001
 	0x02, //0x02 00000010
@@ -9,14 +11,71 @@ static BIT_MASKS: [u8; 8] = [
 	0x80, //0x80 10000000
 ];
 
-fn main() {
-	let mut test = b"aa".to_vec();
-	let mut one = 1;
+//calculate hamming distance between strings
+// fn calc_ham_dist(s1: &str, s2: &str) -> i32{
+// 	if s1.len() != s2.len() {
+// 		return 0;
+// 	}
 
-	for byte in test.iter() {
+// 	let s1_bytes = s1.as_bytes().to_vec();
+// 	let s2_bytes = s2.as_bytes().to_vec();
+// 	let mut hamming_dist: i32 = 0;
+
+// 	println!("{:?}", s1_bytes);
+
+// 	for (byte1, byte2) in s1_bytes.iter().zip(s2_bytes.iter()) {
+// 		for x in 0..8 {
+// 			// println!("val1: {:?} val2: {:?}", (byte1 & BIT_MASKS[x]) >> x, (byte2 & BIT_MASKS[x]) >> x);
+// 			if ((byte1 & BIT_MASKS[x]) >> x) != ((byte2 & BIT_MASKS[x]) >> x) {
+// 				hamming_dist += 1;
+// 			}
+// 		}
+// 	}
+
+// 	return hamming_dist;
+// }
+
+
+fn calc_ham_dist(s1: Vec<u8>, s2: Vec<u8>) -> i32{
+	if s1.len() != s2.len() {
+		return 0;
+	}
+
+	let mut hamming_dist: i32 = 0;
+
+	for (byte1, byte2) in s1.iter().zip(s2.iter()) {
 		for x in 0..8 {
-			println!("mask: {:?} val: {:?}", BIT_MASKS[x], (byte & BIT_MASKS[x]) >> x);
+			// println!("val1: {:?} val2: {:?}", (byte1 & BIT_MASKS[x]) >> x, (byte2 & BIT_MASKS[x]) >> x);
+			if ((byte1 & BIT_MASKS[x]) >> x) != ((byte2 & BIT_MASKS[x]) >> x) {
+				hamming_dist += 1;
+			}
 		}
 	}
+
+	return hamming_dist;
+}
+
+fn find_key_size(input: &str) -> i32 {
+	let input_bytes = base64::decode(&input).unwrap().to_vec();
+
+	// println!("input bytes: {:?} ", input_bytes);
+
+	for x in 2..10 {
+		println!("{:?}", calc_ham_dist(input_bytes[0..x].to_vec(), input_bytes[x..2*x].to_vec()));
+	}
+
+	return 0;
+}
+
+
+//need to find a way to input strings as one line instead of separate lines.. newline character causing error in decoding
+fn main() {
+	// let s1 = "this is a test";
+	// let s2 = "wokka wokka!!!";
+
+	//60 base64 chars per line => 60 chars / 4 chars/3 bytes = 45 bytes
+	let test_str = "HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS";
+
+	find_key_size(&test_str);
 
 }
