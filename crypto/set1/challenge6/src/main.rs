fedafab2061b7837ffb5b4b2c513f4b86573d412
root <root@localhost.localdomain>
2020-07-15 04:47:49 +0000

    can find the length of key

--- crypto/set1/challenge6/src/main.rs
+++ crypto/set1/challenge6/src/main.rs
@@ -1,4 +1,7 @@
 use base64;
+use std::fs::File;
+use std::fs;
+use std::io::{self, prelude::*, BufReader};
 
 static BIT_MASKS: [u8; 8] = [
 	0x01, //0x01 00000001
@@ -35,7 +38,7 @@ static BIT_MASKS: [u8; 8] = [
 // 	return hamming_dist;
 // }
 
-
+//use for two values
 fn calc_ham_dist(s1: Vec<u8>, s2: Vec<u8>) -> i32{
 	if s1.len() != s2.len() {
 		return 0;
@@ -57,25 +60,60 @@ fn calc_ham_dist(s1: Vec<u8>, s2: Vec<u8>) -> i32{
 
 fn find_key_size(input: &str) -> i32 {
 	let input_bytes = base64::decode(&input).unwrap().to_vec();
+	let input_bytes_arr = base64::decode(&input).unwrap();
 
+
+	let mut ham_distance: i32 = 0;
 	// println!("input bytes: {:?} ", input_bytes);
 
-	for x in 2..10 {
-		println!("{:?}", calc_ham_dist(input_bytes[0..x].to_vec(), input_bytes[x..2*x].to_vec()));
+	for x in 2..40 {
+		let mut input_chunks: Vec<&[u8]> = input_bytes_arr.chunks(x).collect();
+
+		for y in 0..4 {
+			for z in 0..4 {
+				ham_distance += calc_ham_dist(input_chunks[y].to_vec(), input_chunks[z].to_vec());
+
+			}
+		}
+
+		ham_distance = (ham_distance / 4) / x as i32;
+
+		// println!("ham distance: {:?}, key_size: {:?}", ham_distance, x);
+
 	}
 
 	return 0;
 }
 
+fn open_file(file_name: &str) -> io::Result<String>{
+	let file = File::open(file_name)?;
+	let reader = BufReader::new(file);
+	let mut cur_line: String = "".to_string();
 
-//need to find a way to input strings as one line instead of separate lines.. newline character causing error in decoding
+	for line in reader.lines() {
+		cur_line = format!("{}{}", cur_line, line.unwrap());
+	}
+
+	// println!("{:?}", cur_line);
+
+	return Ok(cur_line);
+}
+
+//need to change xor_data function from challenge 3 to return the xor character which will correspond to a single character of the key
+//need to run that function x times for length of key, building the key one character at a time
+//change the scoring system to count the raw frequency of character as the best score --> can ignore frequency * english frequency
+
 fn main() {
-	// let s1 = "this is a test";
-	// let s2 = "wokka wokka!!!";
-
+	// let s1 = "this is a test".as_bytes().to_vec();
+	// let s2 = "wokka wokka!!!".as_bytes().to_vec();
+	// let s3 = "this is a test".as_bytes().to_vec();
+	// let s4 = "wokka wokka!!!".as_bytes().to_vec();
 	//60 base64 chars per line => 60 chars / 4 chars/3 bytes = 45 bytes
-	let test_str = "HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS";
+	// let test_str = "HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS"
 
-	find_key_size(&test_str);
+	let file_str = open_file("full_input.txt").unwrap();
+
+	find_key_size(&file_str);
+
 
 }
