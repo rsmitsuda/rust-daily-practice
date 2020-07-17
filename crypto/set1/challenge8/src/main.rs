24e10f0d0700a1cb8f761886847f10378c751939
rsmitsuda <rsmitsuda@gmail.com>
2020-07-17 06:05:27 +0000

    challenge8 complete

--- crypto/set1/challenge8/src/main.rs
+++ crypto/set1/challenge8/src/main.rs
@@ -2,18 +2,31 @@ use hex;
 use std::fs::File;
 use std::fs;
 use std::io::{self, prelude::*, BufReader};
+use std::collections::HashSet;
 
-fn find_aes(input_vec: Vec<String>) {
+//need to find set of values with least amount of unique values => ecb will have repeating blocks of ciphertext when same plaintext is encrypted
+fn find_aes(input_vec: Vec<String>) -> String {
+	let mut unique_vals = 10000;
+	let mut ret_line = "".to_string();
+
 	for line in input_vec {
 		let bytes = hex::decode(line.clone()).unwrap();
-		println!("{:?}", bytes.len());
+		// println!("{:?}", bytes.len());
 
 		let chunks: Vec<&[u8]> = bytes.chunks(16).collect();
-		println!("chunks: {:?}", chunks);
 		//see which chunk has the most repeat values of 16 bytes
+		let chunk_set: HashSet<&[u8]> = 
+			chunks.to_vec()
+			.into_iter()
+			.collect();
 
+		if chunk_set.len() < unique_vals {
+			unique_vals = chunk_set.len();
+			ret_line = line;
+		}
 	}
 
+	return ret_line;
 }
 
 
@@ -31,6 +44,6 @@ fn open_file(input: &str) -> io::Result<Vec<String>> {
 }
 
 fn main() {
-	find_aes(open_file(&"input.txt").unwrap());
+	println!("{:?}", find_aes(open_file(&"input.txt").unwrap()));
 
 }
