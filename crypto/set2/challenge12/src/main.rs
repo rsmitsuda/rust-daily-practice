f99ecd4a6eb6a7eaf5d9612d9bb10123b432069f
rsmitsuda <rsmitsuda@gmail.com>
2020-07-21 04:56:08 +0000

    detect cipher mode

--- crypto/set2/challenge12/src/main.rs
+++ crypto/set2/challenge12/src/main.rs
@@ -2,6 +2,7 @@ use base64;
 use std::default::{Default};
 use rand;
 use openssl::symm::{decrypt, Cipher, encrypt, Crypter, Mode};
+use std::collections::HashSet;
 
 #[derive(Default)]
 struct EcbOracle {
@@ -47,7 +48,7 @@ impl EcbOracle {
 	}
 }
 
-fn find_block_size(oracle: &mut EcbOracle) -> i32 {
+fn find_block_size(oracle: &mut EcbOracle) -> (i32, Vec<u8>) {
 	let mut test_byte = vec![b'A'];
 	let mut ciphertext = oracle.oracle_encrypt(&test_byte);
 	let init_len = ciphertext.len();
@@ -60,13 +61,30 @@ fn find_block_size(oracle: &mut EcbOracle) -> i32 {
 	}
 
 	// println!("cur: {:?}, init: {:?}", cur_len, init_len);
-	return cur_len as i32 - init_len as i32;
+	return (cur_len as i32 - init_len as i32, ciphertext);
 
+}
+
+fn find_aes_mode(input_vec: &Vec<u8>) {
+	let chunks: Vec<&[u8]> = input_vec.chunks(16).collect();
+	let chunk_set: HashSet<&[u8]> = 
+		chunks.to_vec()
+		.into_iter()
+		.collect();
+
+
+
+	println!("{:?}", chunk_set.len());
+	if chunk_set.len() != chunks.len() {
+		println!("{:?}", "ECB");
+	} else {
+		println!("{:?}", "CBC");
+	}
 }
 
 fn main() {
     let unknown = base64::decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK").unwrap();
-    
+    let temp = unknown.clone();
     //create oracle
     let mut oracle = EcbOracle::default();
 
@@ -76,5 +94,12 @@ fn main() {
     //generate unknown key for oracle
     oracle.generate_key();
 
-    println!("blocksize: {:?}", find_block_size(&mut oracle));
+    //print blocksize
+    let (blocksize, ciphertext): (i32, Vec<u8>) = find_block_size(&mut oracle);
+    println!("blocksize: {:?}", blocksize);
+
+    //detect cipher mode
+    let repeat_data = vec![b'0'; 10000];
+    let temp_ciphertext = oracle.oracle_encrypt(&repeat_data);
+    find_aes_mode(&temp_ciphertext);
 }
