6f2cfc6544c78f0eb072ce178fb7929abaf58a31
rsmitsuda <rsmitsuda@gmail.com>
2020-07-25 04:40:57 +0000

    just need to cut paste ciphertext

--- crypto/set2/challenge13/src/main.rs
+++ crypto/set2/challenge13/src/main.rs
@@ -1,6 +1,7 @@
 use std::default::{Default};
 use rand;
 use openssl::symm::{decrypt, Cipher, encrypt, Crypter, Mode};
+use std::str;
 
 #[derive(Default)]
 struct EcbOracle {
@@ -45,13 +46,39 @@ impl EcbOracle {
 
 		ciphertext
 	}
+
+	fn oracle_decrypt(&self, ciphertext: &[u8]) -> String {
+		let mut aes = Crypter::new(Cipher::aes_128_ecb(), Mode::Decrypt, &self.key, None).unwrap();
+		let data_chunks: Vec<&[u8]> = ciphertext.chunks(16).collect();
+		let mut ret_str: String = "".to_string();
+
+		for chunk in data_chunks {
+			let mut ret_thing = vec![0 as u8; chunk.len() + 16];
+			aes.update(&chunk, &mut ret_thing);
+			ret_str = format!("{}{}", ret_str, str::from_utf8(&ret_thing).unwrap());
+			// ret_thing.clear();
+			aes = Crypter::new(Cipher::aes_128_ecb(), Mode::Decrypt, &self.key, None).unwrap();
+		}
+
+		ret_str
+	}
 }
 
 fn kv_parse(cookie: &str) -> String {
 	let temp: Vec<&str> = cookie.split('&').collect();
+	let mut ret_vec: Vec<&str> = Vec::new();
+	// let thingy: Vec<&str> = temp.into_iter().map(|x| x.split('=')).collect();
 	for x in temp.iter() {
-		println!("{:?}", x);
+		// println!("{:?}", x);
+		let temp_vec: Vec<&str> = x.split('=').collect();
+		ret_vec.extend(&temp_vec);
+
 	}
+
+	for i in ret_vec {
+		println!("{:?}", i);
+	}
+	// let ret_str = profile_for(&ret_vec, &"test");
 	let ret_str = profile_for(&"test");
 	return ret_str;
 }
@@ -64,10 +91,45 @@ fn profile_for(email: &str) -> String {
 	// return ret_str.to_string();
 }
 
+//other profile function
+/*fn profile_for(fields: &Vec<&str>) -> String {
+	let prefix = format!("{{\n\t{}: \'", fields[0]);
+	let suffix = format!("\'\n\t{}: {}\n\t{}: \'{}\'\n}}", fields[2], fields[3], fields[4], fields[5]);
+	return format!("{}{}{}", prefix, fields[1], suffix);
+	// return ret_str.to_string();
+}*/
+
+// -------------------------------------------------------------------------
+// email=food@bar.co m&uid=10&role=us er
+// 01234567890123450 0123456789012345 01
+//                   |
+// want to turn into |
+//                   v 
+// need to have role= be the end of the block and admin start at beginning of block
+// will create email long enough to cut, copy, paste blocks to create the new CT
+// 
+// email=XXXXXXXXXX foo@bar.comXXXXX adminXXXXXXXXXXX XXXXXXXXXXXXXXXX XXX&uid=10&role= user
+// 0123456789012345 0123456789012345 0123456789012345 0123456789012345 0123456789012345 01234
+//   B1                    B2              B3              B4               B5
+//  B1 + B2 + B5 + B3
+//--------------------------------------------------------------------------
+
 fn main() {
 	// let mut ret_str = "{\n\temail: ";
 	// println!("{}", ret_str);
 	kv_parse("foo=bar&baz=qux&zap=zazzle");
 
-    println!("{}", profile_for(&"test"));
+	let eng_string = b"email=XXXXXXXXXXfoo@bar.comXXXXXadminXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXuid=10&role=user";
+    let mut oracle = EcbOracle::default();
+    oracle.generate_key();
+
+    let ciphertext= oracle.oracle_encrypt(eng_string);
+    println!("{:?}", ciphertext);
+
+    let first = &ciphertext[0..32];
+    println!("{:?}, {}", first, first.len());
+
+    println!("decrypt: {}", oracle.oracle_decrypt(&first));
+
+    // println!("{}", profile_for(&"test"));
 }
